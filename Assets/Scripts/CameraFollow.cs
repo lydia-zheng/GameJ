7c045c0a151b834f33a0141067a45bd59783e6eb
Yan <ibyanli@gmail.com>
2018-02-10 15:42:03 -0800

    camera follows, and up is jump

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public GameObject target;
+	public float cameraSpeed;
+	private Vector3 targetPos;
+
+	private 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPos = new Vector3 (target.transform.position.x, target.transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, targetPos, cameraSpeed * Time.deltaTime);
+	}
+}
