bb0ab5753f64d15787bc889c0ed0ea61932d8ce8
lydia-zheng <lydiamzheng@gmail.com>
2018-02-10 01:38:16 -0800

    Created a template Game Menu(still needs End and Optional Load Implementations)

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void LoadGameBtn()
+    {
+        //not implemented
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit(); //still need to implement how to deal with this
+    }
+}
