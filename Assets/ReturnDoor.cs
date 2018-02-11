2e5af7a972664de910c0308d815cff2ee163622f
lydia-zheng <lydiamzheng@gmail.com>
2018-02-11 13:44:09 -0800

    Working on alleyway returndoor

--- Assets/ReturnDoor.cs
+++ Assets/ReturnDoor.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ReturnDoor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+
+    public string levelName = "CitySceneExit";
+    private gameMaster gm;
+
+    bool isLoadingScene = false;
+
+    void Start()
+    {
+        gm = FindObjectOfType<gameMaster>();
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.GetComponent<PlayerMovement>())
+        {
+
+            
+            if(isLoadingScene != true)
+            {
+            isLoadingScene = true;
+            SceneManager.LoadScene("CitySceneExit");
+            }
+            
+        }
+    }
+    
+    }
+ 
+
+
+
