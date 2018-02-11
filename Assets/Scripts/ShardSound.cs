0c3dff14f5285a19a667326d587b546a8b915466
Yan <ibyanli@gmail.com>
2018-02-11 14:11:15 -0800

    small fix

--- Assets/Scripts/ShardSound.cs
+++ Assets/Scripts/ShardSound.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShardSound : MonoBehaviour {
-	public AudioSource shardSound;
-	public AudioClip shardSound;
+
 
 	// Use this for initialization
 	void Start () {
