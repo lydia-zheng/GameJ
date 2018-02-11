cc4005fc6ef55ac745e904723a149b6cdab6d112
lydia-zheng <lydiamzheng@gmail.com>
2018-02-10 20:15:38 -0800

    Added Shard Collectible system to TestDoor Scene

--- Assets/Scripts/gameMaster.cs
+++ Assets/Scripts/gameMaster.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class gameMaster : MonoBehaviour {
+    public int shards;
     public Text InputText;
-	
+    public Text ShardText;
+
+    void Update()
+    {
+        ShardText.text = ("Shards: " + shards);
+    }
+
 }
