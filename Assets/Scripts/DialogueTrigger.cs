b98ada0168155cfba0064c5146ac3596e9c11c7d
lydia-zheng <lydiamzheng@gmail.com>
2018-02-11 17:20:59 -0800

    asd

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -61,6 +61,7 @@ public class DialogueTrigger : MonoBehaviour {
 
 			}
 			if (gameObject.name == "Detective") {
+                print("hello");
 				DialogM.displayBox ();
 				DialogM.currNPC = "Detective";
 			}
